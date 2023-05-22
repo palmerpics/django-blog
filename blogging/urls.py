777fd44a76be76f8aa08e87411e452c731785217
Anton Palmer <apalmer@winebid.com>
2023-05-21 17:24:32 -0700

    Update blogging to use the class based views

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from blogging.views import stub_view
-from blogging.views import list_view
-from blogging.views import detail_view
+# from blogging.views import stub_view
+# from blogging.views import list_view
+#from blogging.views import detail_view
+from blogging.views import BloggingListView, BloggingDetailView
 
 urlpatterns = [
-    path('', list_view, name="blog_index"),
-    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
+    #path('', list_view, name="blog_index"),
+    #path('posts/<int:post_id>/', detail_view, name="blog_detail"),
+    path('', BloggingListView.as_view(), name="blog_index"),
+    path('posts/<int:pk>/', BloggingDetailView.as_view(), name="blog_detail"),
 ]
