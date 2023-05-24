8a8b867838a4d089f97a22a0182bb6150a7289f8
Anton Palmer <apalmer@winebid.com>
2023-05-23 17:22:23 -0700

    add restframework addon

---
+++ blogging/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from blogging.models import Post, Category
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title', 'text', 'author', 'created_date', 'modified_date', 'published_date']
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['name', 'description']
+
+
+# class GroupSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Group
+#         fields = ['url', 'name']
